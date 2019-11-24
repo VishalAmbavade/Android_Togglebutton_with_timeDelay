47b10d86ee7f8a11711399f6aed9247286492cf5
VishalAmbavade <vaambavade@gmail.com>
2019-11-24 11:58:21 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/vishal/togglebutton/MainActivity.java
+++ app/src/main/java/com/example/vishal/togglebutton/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
                             toggleButton2.setChecked(false);
                             mp.start();
                         }
-                    }, 40000);
+                    }, 50000);
 
                 } else {
                     Toast.makeText(MainActivity.this, "Uncheck", Toast.LENGTH_SHORT).show();
